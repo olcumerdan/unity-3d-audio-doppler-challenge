3973c500936e3ea4c636a53f49de687b61da450b
olcumerdan <olcumerdan@hotmail.com>
2026-09-21 12:57:44 +0300

    Initial commit - 3D audio and Doppler effect implementation

---
+++ Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FareKontrol : MonoBehaviour
+{
+    public float hassasiyet = 200f; // Farenin dönüş hızı
+    float xDonus = 0f;
+    float yDonus = 0f;
+
+    void Start()
+    {
+        // Oyun başladığında fare imlecini ekrana kilitle ve gizle
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Farenin hareketlerini al
+        float mouseX = Input.GetAxis("Mouse X") * hassasiyet * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * hassasiyet * Time.deltaTime;
+
+        // Yukarı/aşağı bakma açısını hesapla ve sınırla (boyun kırılmasın diye -90 ile 90 arası)
+        xDonus -= mouseY;
+        xDonus = Mathf.Clamp(xDonus, -90f, 90f);
+
+        // Sağa/sola bakma açısını hesapla
+        yDonus += mouseX;
+
+        // Kamerayı yeni açılara göre döndür
+        transform.localRotation = Quaternion.Euler(xDonus, yDonus, 0f);
+    }
+}
---
+++ Assets/Moving.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Araba sanıyede 5 bırım hızla ılerı (Z ekseninde) gıtsın 
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+        
+    }
+}
